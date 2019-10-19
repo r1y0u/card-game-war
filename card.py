eeba7d0579998bdedb492bdb400a27c9b5b50d58
ryu <loved.baske14pride10ryu@gmail.com>
2019-10-20 00:21:51 +0900

    Update #2 class Deck

--- card.py
+++ card.py
@@ -1,12 +1,15 @@
+
+from random import shuffle
+
 class Card:
 
     suits = ["spades", "hearts", "diamonds", "clubs"]
 
-    values = [ None, None,
+    values = [None, None,
                "2", "3", "4", "5", "6", "7", "8", "9", "10",
               "Jack", "Queen", "King", "Ace"]
 
-    def __init__(self, s, v):
+    def __init__(self, v, s):
         self.suit = s
         self.value = v
 
@@ -26,7 +29,7 @@ class Card:
         if self.value > c2.value:
             return True
         if self.value == c2.value:
-            if self.suit > c2.value:
+            if self.suit > c2.suit:
                 return True
             else:
                 return False
@@ -36,3 +39,16 @@ class Card:
         r = self.values[self.value] + " of " \
             + self.suits[self.suit]
         return r
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(0, 4):
+                self.cards.append(i, j)
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
