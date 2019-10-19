fff0d36071e0bcbbbdbde9a04762a42ff523af4e
ryu <loved.baske14pride10ryu@gmail.com>
2019-10-19 18:34:18 +0900

    initial commit

---
+++ card.py
@@ -0,0 +1,7 @@
+class Card:
+
+    suits = ["spades", "hearts", "diamonds", "clubs"]
+
+    values = ["None,", "None", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+              "Jack", "Queen", "King", "Ace"]
+
